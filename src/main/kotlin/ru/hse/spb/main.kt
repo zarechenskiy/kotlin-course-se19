8556d87f96d37a09a3e478afedfcaa3e44280de8
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2018-09-05 19:53:33 +0300

    Introduce base template

---
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -0,0 +1,13 @@
+package ru.hse.spb
+
+fun getGreeting(): String {
+    val words = mutableListOf<String>()
+    words.add("Hello,")
+    words.add("world!")
+
+    return words.joinToString(separator = " ")
+}
+
+fun main(args: Array<String>) {
+    println(getGreeting())
+}
