5146349b58c1df89f64203ead4643a0e461c5e11
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2018-09-13 01:30:05 +0300

    Add antlr example

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,20 @@
 package ru.hse.spb
 
+import org.antlr.v4.runtime.BufferedTokenStream
+import org.antlr.v4.runtime.CharStreams
+import ru.hse.spb.parser.ExpLexer
+import ru.hse.spb.parser.ExpParser
+
 fun getGreeting(): String {
     val words = mutableListOf<String>()
     words.add("Hello,")
+
     words.add("world!")
 
     return words.joinToString(separator = " ")
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val expLexer = ExpLexer(CharStreams.fromString("(1 + 2)"))
+    println(ExpParser(BufferedTokenStream(expLexer)).eval().value)
 }
