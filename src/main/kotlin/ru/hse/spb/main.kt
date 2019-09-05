4d7831117e8064d61eb41e6de14ab8ca65de6fc8
Maxim Zuev <max-zu99@mail.ru>
2019-09-06 00:35:07 +0300

    task done

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,175 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.lang.IllegalArgumentException
+import java.util.*
 
-    return words.joinToString(separator = " ")
+/** Vertex in graph. Contains ids of it's neighbours. */
+data class Vertex(
+    val neighbourIds: MutableList<Int> = mutableListOf(),
+    var used: Boolean = false)
+
+/**
+ * Fixed size undirected graph.
+ */
+class Graph(size: Int) {
+
+    /** Vertices in the graph. */
+    private val vertices: MutableList<Vertex> = mutableListOf()
+
+    /** While searching for a cycle vertices in this list presents a path from the root. */
+    private val pathIds: MutableList<Int> = mutableListOf()
+
+    init {
+        for (i in 1..size) {
+            vertices.add(Vertex())
+        }
+    }
+
+    /**
+     * Get size of the graph.
+     * It equals the number of vertices in the graph.
+     */
+    fun size(): Int {
+        return vertices.size
+    }
+
+    /**
+     * Check if vertex id is valid.
+     * Should be 0 <= id < size.
+     * Throws otherwise.
+     */
+    private fun checkVertexId(id: Int) {
+        if (id < 0 || id >= vertices.size) {
+            throw IllegalArgumentException("Vertex id is out of range.")
+        }
+    }
+
+    /**
+     * Add a new edge between vertices id1 and id2 into the graph.
+     * Throws if ids are invalid.
+     */
+    fun addEdge(id1: Int, id2: Int) {
+        checkVertexId(id1)
+        checkVertexId(id2)
+        vertices[id1].neighbourIds.add(id2)
+        vertices[id2].neighbourIds.add(id1)
+    }
+
+    /**
+     * Remove an edge between vertices id1 and id2.
+     * Throws if ids are invalid.
+     */
+    fun removeEdge(id1: Int, id2: Int) {
+        checkVertexId(id1)
+        checkVertexId(id2)
+        vertices[id1].neighbourIds.remove(id2)
+        vertices[id2].neighbourIds.remove(id1)
+    }
+
+    /**
+     * Finds a cycle in graph and returns a list od vertices' ids in the cycle
+     * in the order of their following in the graph.
+     * The answer is empty if no cycle found.
+     */
+    fun findCycle(): List<Int> {
+        if (vertices.isEmpty()) {
+            return emptyList()
+        }
+        pathIds.clear()
+        for (vertex in vertices) {
+            vertex.used = false
+        }
+
+        return findCycle(vertexId = 0)
+    }
+
+    /** Recursive dfs function for finding a cycle. */
+    private fun findCycle(vertexId: Int, parentId: Int = -1): List<Int> {
+        if (vertices[vertexId].used) {
+            return pathIds.drop(pathIds.indexOf(vertexId))
+        }
+
+        pathIds.add(vertexId)
+        vertices[vertexId].used = true
+
+        val childrenIds = vertices[vertexId].neighbourIds
+            .filter { it != parentId }
+        for (childId in childrenIds) {
+            val result = findCycle(vertexId = childId, parentId = vertexId)
+            if (result.isNotEmpty()) {
+                return result
+            }
+        }
+        pathIds.removeAt(pathIds.size - 1)
+
+        return emptyList()
+    }
+
+    /**
+     * Calculate distances from vertexId vertex to all the reachable vertices.
+     * Writes an answer into distance array.
+     * It is presupposed that distance array is filled with -1 values.
+     */
+    fun calculateDistances(vertexId: Int, distance: IntArray) {
+        val queue = mutableListOf<Int>()
+        queue.add(vertexId)
+        distance[vertexId] = 0
+
+        while (queue.isNotEmpty()) {
+            val id = queue.removeAt(0)
+            for (neighbourId in vertices[id].neighbourIds) {
+                if (distance[neighbourId] == -1) {
+                    distance[neighbourId] = distance[id] + 1
+                    queue.add(neighbourId)
+                }
+            }
+        }
+    }
 }
 
+/** Read info from input and create graph. */
+fun createGraph(input: Scanner): Graph {
+    val size = input.nextInt()
+    val graph = Graph(size)
+
+    for (i in 1..size) {
+        val id1 = input.nextInt() - 1
+        val id2 = input.nextInt() - 1
+        graph.addEdge(id1, id2)
+    }
+
+    return graph
+}
+
+/**
+ * Find distances from the cycle for all the vertices in the graph.
+ * It is presupposed that cycle exists.
+ */
+fun solveTask(input: Scanner): IntArray {
+    val graph = createGraph(input)
+    val distance = IntArray(graph.size()) { -1 }
+
+    val cycleIds = graph.findCycle()
+
+    for (i in cycleIds.indices) {
+        if (i == 0) {
+            graph.removeEdge(cycleIds[0], cycleIds.last())
+        } else {
+            graph.removeEdge(cycleIds[i], cycleIds[i - 1])
+        }
+    }
+
+    for (id in cycleIds) {
+        graph.calculateDistances(id, distance)
+    }
+
+    return distance
+}
+
+
 fun main() {
-    println(getGreeting())
+    val input = Scanner(System.`in`)
+    val answer = solveTask(input)
+    val answerString = answer.joinToString(" ")
+    println(answerString)
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -2,10 +2,86 @@ package ru.hse.spb
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.*
 
-class TestSource {
-    @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+// http://codeforces.com/contest/131/submission/60143264
+
+class TestTaskSolver {
+
+    private fun test(input: String, output: String) {
+        val inScanner = Scanner(input)
+        val answer = solveTask(inScanner)
+        assertEquals(output, answer.joinToString(" "))
     }
+
+    private fun testDistance(input: String, output: String, vertexId: Int = 0) {
+        val inScanner = Scanner(input)
+        val graph = createGraph(inScanner)
+        val distance = IntArray(graph.size()) { -1 }
+
+        graph.calculateDistances(vertexId, distance)
+
+        assertEquals(output, distance.joinToString(" "))
+    }
+
+    @Test
+    fun testSolveTask1() {
+        test("4 1 3 4 3 4 2 1 2", "0 0 0 0")
+    }
+
+    @Test
+    fun testSolveTask2() {
+        test("6 1 2 3 4 6 4 2 3 1 3 3 5", "0 0 0 1 1 2")
+    }
+
+    @Test
+    fun testSolveTask3() {
+        test("3 1 2 1 3 2 3", "0 0 0")
+    }
+
+    @Test
+    fun testSolveTask4() {
+        test("4 3 1 3 4 2 1 4 2", "0 0 0 0")
+    }
+
+
+    @Test
+    fun testSolveTask5() {
+        test("4 4 3 1 3 2 1 2 3", "0 0 0 1")
+    }
+
+    @Test
+    fun testCalculateDistance1() {
+        testDistance("4 1 3 4 3 4 2 1 2", "0 1 1 2")
+    }
+
+    @Test
+    fun testCalculateDistance2() {
+        testDistance("6 1 2 3 4 6 4 2 3 1 3 3 5", "0 1 1 2 2 3")
+    }
+
+    @Test
+    fun testCalculateDistance3() {
+        testDistance("3 1 2 1 3 2 3", "0 1 1")
+    }
+
+    @Test
+    fun testCalculateDistance4() {
+        testDistance("4 3 1 3 4 2 1 4 2", "0 1 1 2")
+    }
+
+
+    @Test
+    fun testCalculateDistance5() {
+        testDistance("4 4 3 1 3 2 1 2 3", "0 1 1 2")
+    }
+
+    @Test
+    fun testCalculateDistance6() {
+        testDistance(
+            "14 2 5 5 1 5 3 6 5 4 5 5 7 8 7 7 14 9 7 13 9 9 10 12 9 1 10 10 11",
+            "1 3 3 3 2 3 2 3 1 0 1 2 2 3",
+            9)
+    }
+
 }
