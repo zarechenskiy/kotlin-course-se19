0371923481fab61bada9e0063a651fe64c8397dc
Gavrilov <gavrilov_dan@mail.ru>
2019-09-06 01:29:07 +0300

    hw01 done

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,50 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.lang.StrictMath.*
+import java.util.*
 
-    return words.joinToString(separator = " ")
+var epsilon: Double = 1e-7
+
+class Point(var x: Double = 0.0, var y: Double = 0.0)
+
+fun distance(first: Point, second: Point): Double {
+    return sqrt((first.x - second.x) * (first.x - second.x) + (first.y - second.y) * (first.y - second.y))
+}
+
+fun isReachable(start: Point, finish: Point,
+                coordinateDifferenceX: Double, coordinateDifferenceY: Double,
+                time: Double, speed: Double): Boolean {
+    val finishConsideringWind = Point(finish.x - coordinateDifferenceX, finish.y - coordinateDifferenceY)
+    return speed * time >= distance(start, finishConsideringWind)
+}
+
+fun solve(input: Scanner): Double {
+    val start = Point()
+    start.x = input.nextDouble()
+    start.y = input.nextDouble()
+    val finish = Point()
+    finish.x = input.nextDouble()
+    finish.y = input.nextDouble()
+    val speed = input.nextDouble()
+    val time = input.nextDouble()
+    val firstWindSpeedX = input.nextDouble()
+    val firstWindSpeedY = input.nextDouble()
+    val secondWindSpeedX = input.nextDouble()
+    val secondWindSpeedY = input.nextDouble()
+
+    var low = 0.0
+    var high = 1e18
+    while (high - low > epsilon) {
+        val middle = (low + high) / 2
+        val coordinateDifferenceX = firstWindSpeedX * min(time, middle) + secondWindSpeedX * max(0.0, middle - time)
+        val coordinateDifferenceY = firstWindSpeedY * min(time, middle) + secondWindSpeedY * max(0.0, middle - time)
+        if (isReachable(start, finish, coordinateDifferenceX, coordinateDifferenceY, middle, speed)) high = middle
+        else low = middle
+    }
+
+    return low
 }
 
 fun main() {
-    println(getGreeting())
+    println(solve(Scanner(System.`in`)))
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,35 @@
 package ru.hse.spb
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
+import java.lang.StrictMath.abs
+import java.lang.StrictMath.max
+import java.util.*
 
 class TestSource {
-    @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+
+    private fun checkAnswer(expected: Double, actual: Double): Boolean {
+        return abs(actual - expected) / max(1.0, expected) <= 1e-6
     }
+
+    @Test
+    fun testSolutionBySample1() {
+        val sample = "0 0 5 5\n" +
+                "3 2\n" +
+                "-1 -1\n" +
+                "-1 0"
+        val expectedAnswer = 3.729935587093555327
+        assertTrue(checkAnswer(solve(Scanner(sample)), expectedAnswer))
+    }
+
+    @Test
+    fun testSolutionBySample2() {
+        val sample = "0 0 0 1000\n" +
+                "100 1000\n" +
+                "-50 0\n" +
+                "50 0"
+        val expectedAnswer = 11.547005383792516398
+        assertTrue(checkAnswer(solve(Scanner(sample)), expectedAnswer))
+    }
+
 }
