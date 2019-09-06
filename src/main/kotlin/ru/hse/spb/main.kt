8e1a073e030bd9ab2d67f3bdc278b219711b387a
Vitalii Selishchev <vvselishchev@yandex-team.ru>
2019-09-06 23:37:14 +0300

    Added task solution.

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,128 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
-    return words.joinToString(separator = " ")
+class SubwayGraph {
+    private enum class DfsTraverseType {
+        NOT_VISITED, IN_PROGRESS, FINISHED
+    }
+
+    private data class Station(val id: Int,
+                               var traverseType: DfsTraverseType = DfsTraverseType.NOT_VISITED,
+                               var onCycle: Boolean = false,
+                               var parentIdOnTraversePath: Station? = null)
+
+    private val stations: MutableMap<Int, Station> = HashMap()
+    private val adjacentStations: MutableMap<Int, MutableList<Station>> = HashMap()
+
+    fun addStationIfNotExist(vararg stationIds: Int) {
+        stationIds.forEach { id -> stations.putIfAbsent(id, Station(id)) }
+    }
+
+    fun addNotDirectedTransition(firstStationId: Int, secondStationId: Int) {
+        addDirectedTransitions(firstStationId, secondStationId)
+        addDirectedTransitions(secondStationId, firstStationId)
+    }
+
+    fun addDirectedTransitions(fromId: Int, toId: Int) {
+        addStationIfNotExist(fromId, toId)
+
+        val toStation = stations[toId]
+        adjacentStations.putIfAbsent(fromId, ArrayList())
+        adjacentStations[fromId]?.add(toStation!!)
+    }
+
+    fun getDistancesFromRingStations(): Map<Int, Int> {
+        val distancesFromRing = HashMap<Int, Int>()
+        markRingStationsDfs(stations.values.first())
+        stations.values
+                .filter { station -> station.onCycle }
+                .forEach { station -> distancesFromRing.putAll(getDistancesFromRingStation(station)) }
+        return distancesFromRing
+    }
+
+    private fun getDistancesFromRingStation(currentStation: Station,
+                                            previousStation: Station? = null,
+                                            currentDistance: Int = 0): Map<Int, Int> {
+        val distances = HashMap<Int, Int>()
+        distances[currentStation.id] = currentDistance
+
+        getAdjacentStations(currentStation.id)
+                .filter { adjacentStation -> adjacentStation.id != previousStation?.id }
+                .filterNot { adjacentStation -> adjacentStation.onCycle}
+                .forEach { adjacentStation ->
+                    distances.putAll(getDistancesFromRingStation(adjacentStation,
+                            currentStation, currentDistance + 1)) }
+        return distances
+    }
+
+    private fun getAdjacentStations(vertexId: Int): List<Station> {
+        if (!adjacentStations.containsKey(vertexId)) {
+            throw RuntimeException("Vertex with id=$vertexId is not presented in the graph.")
+        }
+        return adjacentStations[vertexId]!!
+    }
+
+    private fun markRingStationsDfs(currentStation: Station): Boolean {
+        currentStation.traverseType = DfsTraverseType.IN_PROGRESS
+
+        getAdjacentStations(currentStation.id)
+                .filter { adjacentStation -> currentStation.parentIdOnTraversePath?.id != adjacentStation.id }
+                .forEach { adjacentStation ->
+                    when (adjacentStation.traverseType) {
+                        DfsTraverseType.NOT_VISITED -> {
+                            adjacentStation.parentIdOnTraversePath = currentStation
+                            if (markRingStationsDfs(adjacentStation)) {
+                                return true
+                            }
+                        }
+
+                        DfsTraverseType.IN_PROGRESS -> {
+                            markRingStations(currentStation, adjacentStation)
+                            return true
+                        }
+
+                        DfsTraverseType.FINISHED -> return false
+                    }
+                }
+
+        currentStation.traverseType = DfsTraverseType.FINISHED
+        return false
+    }
+
+    private fun markRingStations(startStation: Station, endStation: Station) {
+        var currentVertex = startStation
+        while (currentVertex != endStation) {
+            currentVertex.onCycle = true
+            currentVertex = currentVertex.parentIdOnTraversePath!!
+        }
+        endStation.onCycle = true
+    }
+}
+
+
+fun readSubwayGraph(): SubwayGraph {
+    val scanner = Scanner(System.`in`)
+
+    val graph = SubwayGraph()
+
+    val transitionsCount = scanner.nextInt()
+    for (i in 1..transitionsCount) {
+        graph.addNotDirectedTransition(scanner.nextInt(), scanner.nextInt())
+    }
+
+    return graph
+}
+
+fun printDistances(distances: Map<Int, Int>) {
+    for (i in 1..distances.size) {
+        print("${distances[i]} ")
+    }
 }
 
 fun main() {
-    println(getGreeting())
+    val graph = readSubwayGraph()
+    printDistances(graph.getDistancesFromRingStations())
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -5,7 +5,39 @@ import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testAllStationOnRing() {
+        val graph = SubwayGraph()
+        graph.addNotDirectedTransition(1, 3)
+        graph.addNotDirectedTransition(3, 4)
+        graph.addNotDirectedTransition(4, 2)
+        graph.addNotDirectedTransition(2, 1)
+
+        val distances = graph.getDistancesFromRingStations()
+
+        assertEquals(4, distances.size)
+        for (id in 1..4) {
+            assertEquals(0, distances[id])
+        }
+    }
+
+    @Test
+    fun testRingWithTwoBranchesFromOneStation() {
+        val graph = SubwayGraph()
+        graph.addNotDirectedTransition(1, 2)
+        graph.addNotDirectedTransition(2, 3)
+        graph.addNotDirectedTransition(3, 1)
+        graph.addNotDirectedTransition(3, 5)
+        graph.addNotDirectedTransition(3, 4)
+        graph.addNotDirectedTransition(4, 6)
+
+        val distances = graph.getDistancesFromRingStations()
+
+        assertEquals(6, distances.size)
+        for (id in 1..3)
+            assertEquals(0, distances[id])
+        for (id in 4..5) {
+            assertEquals(1, distances[id])
+        }
+        assertEquals(2, distances[6])
     }
 }
