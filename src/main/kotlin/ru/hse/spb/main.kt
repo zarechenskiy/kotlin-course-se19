3a23e65f65947156916ff04d306206ac786049df
akhoroshev <arthoroshev@gmail.com>
2019-09-07 17:46:12 +0300

    Complete

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,110 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.io.InputStream
+import java.io.OutputStream
+import java.io.PrintWriter
+import java.util.*
+import java.util.stream.Collectors
 
-    return words.joinToString(separator = " ")
+
+typealias Vertex = Int
+typealias Graph = Map<Vertex, List<Vertex>>
+
+
+fun findClosedPathImpl(
+    currentVertex: Vertex,
+    previousVertex: Vertex?,
+    visitedVertex: MutableSet<Vertex>,
+    path: MutableList<Vertex>,
+    graph: Graph
+): List<Vertex>? {
+    path.add(currentVertex)
+    if (visitedVertex.contains(currentVertex)) {
+        return path
+    }
+    visitedVertex.add(currentVertex)
+    for (neighbour in graph.getValue(currentVertex)) {
+        if (neighbour == previousVertex) continue
+        val rec = findClosedPathImpl(neighbour, currentVertex, visitedVertex, path, graph)
+        if (rec != null) return rec
+    }
+    path.removeAt(path.size - 1)
+    return null
+}
+
+fun findClosedPath(startVertex: Vertex, graph: Graph): List<Vertex>? {
+    return findClosedPathImpl(startVertex, null, mutableSetOf(), mutableListOf(), graph)
+}
+
+fun extractCycleFromClosedPath(path: List<Vertex>): Set<Vertex> {
+    val cycle = mutableSetOf<Vertex>()
+    for (vertex in path.reversed()) {
+        if (cycle.contains(vertex)) break
+        cycle.add(vertex)
+    }
+    return cycle
+}
+
+fun findDistancesImpl(
+    currentVertex: Vertex,
+    previousVertex: Vertex?, cycle: Set<Vertex>, distances: MutableMap<Vertex, Int>, currentDistance: Int, graph: Graph
+) {
+    distances[currentVertex] = currentDistance
+    for (neighbour in graph.getValue(currentVertex)) {
+        if (neighbour == previousVertex) continue
+        if (cycle.contains(neighbour)) continue
+        findDistancesImpl(neighbour, currentVertex, cycle, distances, currentDistance + 1, graph)
+    }
+}
+
+fun findDistances(cycle: Set<Vertex>, graph: Graph): Map<Vertex, Int> {
+    val distances = mutableMapOf<Vertex, Int>()
+    for (vertex in cycle) {
+        findDistancesImpl(vertex, null, cycle, distances, 0, graph)
+    }
+    return distances
+}
+
+fun parseInput(inputStream: InputStream): Graph {
+    val scanner = Scanner(inputStream)
+    val size = scanner.nextInt()
+
+    val graph = mutableMapOf<Vertex, MutableList<Vertex>>()
+
+    for (i in 1..size) {
+        graph[i] = mutableListOf()
+    }
+
+    repeat(size) {
+        val i = scanner.nextInt()
+        val j = scanner.nextInt()
+        graph[i]?.add(j)
+        graph[j]?.add(i)
+    }
+
+    return graph
+}
+
+fun putResult(distances: Map<Vertex, Int>, stream: OutputStream) {
+    val writer = PrintWriter(stream)
+    writer.use {
+        writer.println(
+            distances.entries.stream().sorted(compareBy(Map.Entry<Vertex, Int>::key)).map(Map.Entry<Vertex, Int>::value).map(
+                Int::toString
+            ).collect(Collectors.joining(" "))
+        )
+    }
+}
+
+fun task(input: InputStream, output: OutputStream) {
+    val subway = parseInput(input)
+    val cycle = extractCycleFromClosedPath(
+        findClosedPath(startVertex = 1, graph = subway)
+            ?: error("The scheme must contain a cycle")
+    )
+    putResult(findDistances(cycle = cycle, graph = subway), output)
 }
 
 fun main() {
-    println(getGreeting())
+    task(System.`in`, System.out)
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,54 @@
 package ru.hse.spb
 
-import org.junit.Assert.assertEquals
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
 
 class TestSource {
+    private fun testRunner(input: String, output: String) {
+        val inputStream = ByteArrayInputStream(input.toByteArray(Charsets.UTF_8))
+        val outputStream = ByteArrayOutputStream()
+        task(inputStream, outputStream)
+        assertEquals(output, String(outputStream.toByteArray(), Charsets.UTF_8))
+    }
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testCase0() {
+        testRunner(
+            "8\n" +
+                    "1 2\n" +
+                    "2 3\n" +
+                    "3 1\n" +
+                    "3 4\n" +
+                    "4 5\n" +
+                    "5 6\n" +
+                    "6 7\n" +
+                    "6 8", "0 0 0 1 2 3 4 4\n"
+        )
+    }
+
+    @Test
+    fun testCase1() {
+        testRunner(
+            "5\n" +
+                    "2 5\n" +
+                    "4 5\n" +
+                    "4 3\n" +
+                    "4 2\n" +
+                    "1 4", "1 0 1 0 0\n"
+        )
+    }
+
+    @Test
+    fun testCase2() {
+        testRunner(
+            "4\n" +
+                    "4 3\n" +
+                    "1 3\n" +
+                    "2 1\n" +
+                    "2 3", "0 0 0 1\n"
+        )
     }
 }
