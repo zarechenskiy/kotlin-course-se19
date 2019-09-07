3a78f78341d839eac38b9f51058793ab3a50948e
Alexey Trilis <aatrilis2000@gmail.com>
2019-09-07 11:09:45 +0300

    HW1 Done

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,91 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.io.InputStream
+import java.io.OutputStream
+import java.io.PrintWriter
+import java.util.*
 
-    return words.joinToString(separator = " ")
+private data class Station(
+    val id: Int,
+    val adjacentStations: MutableList<Station>,
+    var visited: Boolean = false,
+    var distanceToCircle: Int? = null
+)
+
+private fun readDataset(inputStream: InputStream): Array<Station> {
+    val scanner = Scanner(inputStream)
+
+    val stationCount = scanner.nextInt()
+    val stations = Array(stationCount) {
+        Station(it, mutableListOf())
+    }
+
+    repeat(stationCount) {
+        val from = scanner.nextInt() - 1
+        val to = scanner.nextInt() - 1
+        stations[from].adjacentStations.add(stations[to])
+        stations[to].adjacentStations.add(stations[from])
+    }
+
+    scanner.close()
+    return stations
+}
+
+private fun dfs(station: Station, stationFrom: Station?, path: ArrayDeque<Station>): Boolean {
+    path.addLast(station)
+    station.visited = true
+    station.adjacentStations.forEach {
+        if (it != stationFrom) {
+            if (!it.visited) {
+                if (dfs(it, station, path)) {
+                    return true
+                }
+            } else {
+                path.addLast(it)
+                return true
+            }
+        }
+    }
+    path.removeLast()
+    return false
+}
+
+private fun findCircle(station: Station): List<Station> {
+    val path = ArrayDeque<Station>()
+    dfs(station, null, path)
+    return path.dropWhile { it.id != path.last.id }
+}
+
+private fun computeDistancesToCircle(circle: List<Station>) {
+    val bfsQueue = ArrayDeque<Station>()
+    bfsQueue += circle
+    circle.forEach { it.distanceToCircle = 0 }
+
+    while (bfsQueue.isNotEmpty()) {
+        val currentStation = bfsQueue.pop()
+        currentStation.adjacentStations.forEach {
+            if (it.distanceToCircle == null) {
+                it.distanceToCircle = currentStation.distanceToCircle!! + 1
+                bfsQueue += it
+            }
+        }
+    }
+}
+
+private fun outputResult(stations: Array<Station>, outputStream: OutputStream) {
+    val writer = PrintWriter(outputStream)
+    writer.println(
+        stations.map { it.distanceToCircle!! }.joinToString(separator = " ")
+    )
+    writer.close()
+}
+
+fun executeSolution(inputStream: InputStream, outputStream: OutputStream) {
+    val stations = readDataset(inputStream)
+    computeDistancesToCircle(findCircle(stations[0]))
+    outputResult(stations, outputStream)
 }
 
 fun main() {
-    println(getGreeting())
+    executeSolution(System.`in`, System.out)
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -2,10 +2,32 @@ package ru.hse.spb
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.*
 
 class TestSource {
+
+    private val testDirectory = "src/test/data"
+
+    private fun doTest(testName: String) {
+        val stream = ByteArrayOutputStream()
+        executeSolution(FileInputStream("$testDirectory/$testName.in"), stream)
+        assertEquals(
+            String(FileInputStream("$testDirectory/$testName.out").readAllBytes()),
+            stream.toString()
+        )
+    }
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testSample1() {
+        doTest("sample1")
+    }
+
+    @Test
+    fun testSample2() {
+        doTest("sample2")
     }
 }
