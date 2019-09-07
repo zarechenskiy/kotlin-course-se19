cb978fec914b40bede87e951215c4c0f49891ff2
Konstantin Lyubort <lyubortk@gmail.com>
2019-09-07 20:37:52 +0300

    complete hw1

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,102 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+private const val WRONG_INPUT_MESSAGE = "Wrong input"
 
-    return words.joinToString(separator = " ")
+/**
+ * Reads stdin and solves Codeforces 59C problem (https://codeforces.com/problemset/problem/59/C)
+ * using [ru.hse.spb.Solution]
+ */
+fun main() {
+    val numberOfRequiredLetters = readLine()?.toIntOrNull()
+    val pattern = readLine()
+
+    if (numberOfRequiredLetters == null || pattern == null) {
+        println(WRONG_INPUT_MESSAGE)
+        return
+    }
+
+    println(Solution.solve(numberOfRequiredLetters, pattern))
 }
 
-fun main() {
-    println(getGreeting())
+/**
+ * This class solves Codeforces 59C problem: https://codeforces.com/problemset/problem/59/C
+ */
+class Solution private constructor(numberOfRequiredLetters: Int,
+                                           private val pattern: String) {
+
+    private val requiredCharacterRange = FIRST_LETTER until FIRST_LETTER + numberOfRequiredLetters
+    private val resultArray = Array(pattern.length) { ANY_LETTER }
+
+    private fun prepareResultArray() {
+        for (i in pattern.indices) {
+            if (pattern[i] != ANY_LETTER) {
+                resultArray[i] = pattern[i]
+            } else {
+                resultArray[i] = pattern[pattern.lastIndex - i]
+            }
+        }
+    }
+
+    private fun getLettersNotContainedInPattern() : HashSet<Char> {
+        val notContainedLetters = requiredCharacterRange.toHashSet()
+        notContainedLetters.removeAll(pattern.toList())
+        return notContainedLetters
+    }
+
+    private fun resultArrayIsCorrect() : Boolean {
+        for (i in 0..(resultArray.size / 2)) {
+            if (resultArray[i] != resultArray[resultArray.lastIndex - i]) {
+                return false
+            }
+            if (resultArray[i] !in requiredCharacterRange) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun <E> MutableList<E>.popBack() : E? {
+        if (size > 0) {
+            val lastElement = last()
+            removeAt(lastIndex)
+            return lastElement
+        }
+        return null
+    }
+
+    private fun solveImpl() : String {
+        prepareResultArray()
+
+        val notContainedLetters = getLettersNotContainedInPattern().sorted().toMutableList()
+        for (i in (resultArray.size - 1) / 2 downTo 0) {
+            if (resultArray[i] != ANY_LETTER) {
+                continue
+            }
+            val letterToWrite = notContainedLetters.popBack() ?: FIRST_LETTER
+            resultArray[i] = letterToWrite
+            resultArray[resultArray.lastIndex - i] = letterToWrite
+        }
+
+        if (notContainedLetters.isNotEmpty() || !resultArrayIsCorrect()) {
+            return IMPOSSIBLE_MESSAGE
+        }
+        return resultArray.joinToString(separator = "")
+    }
+
+    companion object {
+        /**
+         * This string will be returned if solution does not exist
+         */
+        const val IMPOSSIBLE_MESSAGE = "IMPOSSIBLE"
+
+        private const val ANY_LETTER = '?'
+        private const val FIRST_LETTER = 'a'
+
+        /**
+         * Solves the problem and returns the answer
+         */
+        fun solve(numberOfRequiredLetters: Int, pattern: String) : String {
+           return Solution(numberOfRequiredLetters, pattern).solveImpl()
+        }
+    }
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -5,7 +5,74 @@ import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testFromCodeforces1() {
+        assertEquals(
+                Solution.IMPOSSIBLE_MESSAGE,
+                Solution.solve(3, "a?c")
+        )
+    }
+
+    @Test
+    fun testFromCodeforces2() {
+        assertEquals(
+                "abba",
+                Solution.solve(2, "a??a")
+        )
+    }
+
+    @Test
+    fun testFromCodeforces3() {
+        assertEquals(
+                "abba",
+                Solution.solve(2, "?b?a")
+        )
+    }
+
+    @Test
+    fun testPatternContainsWrongLetter() {
+        assertEquals(
+                Solution.IMPOSSIBLE_MESSAGE,
+                Solution.solve(2, "abcba")
+        )
+    }
+
+    @Test
+    fun testPatternCouldNotBeTurnedIntoAPalindrome() {
+        assertEquals(
+                Solution.IMPOSSIBLE_MESSAGE,
+                Solution.solve(2, "ababba")
+        )
+    }
+
+    @Test
+    fun testAnswerCouldNotContainAllRequiredLetters() {
+        assertEquals(
+                Solution.IMPOSSIBLE_MESSAGE,
+                Solution.solve(4, "??a??")
+        )
+    }
+
+    @Test
+    fun testComplementsPatternRight() {
+        assertEquals(
+                "dacbbcad",
+                Solution.solve(4, "dacb????")
+        )
+        assertEquals(
+                "dacbbcad",
+                Solution.solve(4, "????bcad")
+        )
+        assertEquals(
+                "dacbbcad",
+                Solution.solve(4, "d?c?b?a?")
+        )
+    }
+
+    @Test
+    fun testPutsAllRequiredLettersInRightOrder() {
+        assertEquals(
+                "aaaaabcbaaaaa",
+                Solution.solve(3,"?????????????")
+        )
     }
 }
