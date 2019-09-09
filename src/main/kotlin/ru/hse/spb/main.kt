11ff714918b1cec9846a245b33cb75950587f6c4
Arseny Babushkin <aytelquant@yandex-team.ru>
2019-09-09 23:49:04 +0500

    impl code & tests

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,70 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.lang.Integer.max
 
-    return words.joinToString(separator = " ")
+class Solver {
+    private val n: Int
+    private val v: IntArray
+    private val q: Int
+    private val queries: Array<IntArray>
+    val ans: IntArray
+
+    constructor() {
+        n = readLine()!!.toInt()
+        v = readLine()!!.split(' ').map(String::toInt).toIntArray()
+        q = readLine()!!.toInt()
+        queries = Array(q) { readLine()!!.split(' ').map(String::toInt).toIntArray()}
+        ans = IntArray(n)
+    }
+
+    internal constructor(n: Int, v: IntArray, q: Int, queries: Array<IntArray>) {
+        this.n = n
+        this.v = v
+        this.q = q
+        this.queries = queries
+        ans = IntArray(n)
+    }
+
+    fun solve() {
+        val last = List(n) {-1}.toMutableList()
+        val ops: MutableList<Pair<Int,Int>> = emptyList<Pair<Int,Int>>().toMutableList()
+
+        val get = fun (j: Int): Int {
+            if (ops.size == 0)
+                return v[j]
+            var l = -1
+            var r = ops.size
+            while (r - l > 1) {
+                val m = (l + r) / 2
+                if (ops[m].first > last[j])
+                    r = m
+                else
+                    l = m
+            }
+            return if (r == ops.size) v[j] else max(v[j], ops[r].second)
+        }
+
+        for (i in IntRange(0, q - 1)) {
+            val cur = queries[i]
+            if (cur[0] == 1) {
+                val j = cur[1] - 1
+                val x = cur[2]
+                v[j] = x
+                last[j] = i
+            } else {
+                val x = cur[1]
+                while (ops.size > 0 && (ops.last().second < x))
+                    ops.removeAt(ops.size - 1)
+                ops.add(Pair(i, x))
+            }
+        }
+
+        IntRange(0, n - 1).forEach { ans[it] = get(it) }
+    }
 }
 
 fun main() {
-    println(getGreeting())
+    val solver = Solver()
+    solver.solve()
+    solver.ans.forEach { print("$it ") }
 }
---
+++ src/test/kotlin/ru/hse/spb/SolverTest.kt
@@ -0,0 +1,38 @@
+package ru.hse.spb
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class SolverTest {
+    @Test
+    fun testFirstSample() {
+        val solver = Solver(
+                4,
+                arrayOf(1, 2, 3, 4).toIntArray(),
+                3,
+                arrayOf(
+                        arrayOf(2, 3).toIntArray(),
+                        arrayOf(1, 2, 2).toIntArray(),
+                        arrayOf(2, 1).toIntArray()
+                )
+        )
+        solver.solve()
+        assertArrayEquals(arrayOf(3, 2, 3, 4).toIntArray(), solver.ans)
+    }
+
+    @Test
+    fun testSecondSample() {
+        val solver = Solver(
+                5,
+                arrayOf(3, 50, 2, 1, 10).toIntArray(),
+                3,
+                arrayOf(
+                        arrayOf(1, 2, 0).toIntArray(),
+                        arrayOf(2, 8).toIntArray(),
+                        arrayOf(1, 3, 20).toIntArray()
+                )
+        )
+        solver.solve()
+        assertArrayEquals(arrayOf(8, 8, 20, 8, 10).toIntArray(), solver.ans)
+    }
+}
