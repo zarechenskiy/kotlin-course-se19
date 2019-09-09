d7ed80a644e3083d797e82aa59e0b842c2956fb6
Irina Pluralia <irina-pluralia@rambler.ru>
2019-09-10 00:39:49 +0300

    add task 1

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,48 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.BufferedReader
 
-    return words.joinToString(separator = " ")
+fun dist(x1: Double, y1: Double, x2: Double, y2: Double) = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+
+fun readDoubles(reader: BufferedReader) = reader.readLine()!!.split(" ").map { it.toDouble() }
+
+fun solver(inputStream: InputStream): Double {
+    val reader = BufferedReader(InputStreamReader(inputStream))
+    val (x1, y1, x2, y2) = readDoubles(reader)
+    val (vMax, t) = readDoubles(reader)
+    val (vx, vy) = readDoubles(reader)
+    val (wx, wy) = readDoubles(reader)
+    reader.close()
+
+    var l = 0.0
+    var r = 1e9
+    for (i in 1..200) {
+        val mid: Double = (l + r) / 2
+
+        val p1: Double
+        val p2: Double
+        if (mid <= t) {
+            p1 = mid
+            p2 = 0.0
+        } else {
+            p1 = t
+            p2 = mid - t
+        }
+
+        val tx = x2 - p1 * vx - p2 * wx
+        val ty = y2 - p1 * vy - p2 * wy
+
+        if (vMax * mid >= dist(tx, ty, x1, y1)) {
+            r = mid
+        } else {
+            l = mid
+        }
+    }
+    return l
 }
 
 fun main() {
-    println(getGreeting())
+    println(solver(System.`in`))
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,41 @@
 package ru.hse.spb
 
+import java.io.ByteArrayInputStream
+import java.nio.charset.Charset
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testSolver1() {
+        val input =
+            "0 0 5 5\n" +
+                    "3 2\n" +
+                    "-1 -1\n" +
+                    "-1 0\n"
+        val inputStream = input.byteInputStream(Charsets.UTF_8)
+        assertEquals(3.729935587093554, solver(inputStream), 1e-6)
+    }
+
+    @Test
+    fun testSolver2() {
+        val input =
+            "0 0 0 1000\n" +
+                    "100 1000\n" +
+                    "-50 0\n" +
+                    "50 0\n"
+        val inputStream = input.byteInputStream(Charsets.UTF_8)
+        assertEquals(11.547005383792516398, solver(inputStream), 1e-6)
+    }
+
+    @Test
+    fun testSolver3() {
+        val input =
+            "-753 8916 -754 8915\n" +
+                    "1000 33\n" +
+                    "999 44\n" +
+                    "-44 -999\n"
+        val inputStream = input.byteInputStream(Charsets.UTF_8)
+        assertEquals(33.000003244932003099, solver(inputStream), 1e-6)
     }
 }
