3a03d9744b1865a8c8769ec50e0d22fd9dc77a5d
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2019-09-10 23:45:01 +0300

    first homework

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,82 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import kotlin.random.Random
 
-    return words.joinToString(separator = " ")
+private fun read() = readLine()!!
+private fun readInt() = read().toInt()
+private fun readStrings() = read().split(' ')
+
+data class TreapNode(val value: Int, val priority: Int = Random.nextInt()) {
+    var left: TreapNode? = null
+    var right: TreapNode? = null
+    var size: Int = 1
+
+    fun update(): TreapNode {
+        size = 1 + left.size + right.size
+        return this
+    }
+}
+
+val TreapNode?.size: Int
+    get() = this?.size ?: 0
+
+fun TreapNode?.split(position: Int): Pair<TreapNode?, TreapNode?> = when {
+    this == null -> null to null
+    left.size < position -> {
+        val (less, greater) = right.split(position - left.size - 1)
+        right = less
+        this.update() to greater
+    }
+    else -> {
+        val (less, greater) = left.split(position)
+        left = greater
+        less to this.update()
+    }
+}
+
+fun merge(node1: TreapNode?, node2: TreapNode?): TreapNode? = when {
+    node1 == null || node2 == null -> node1 ?: node2
+    node1.priority > node2.priority -> {
+        node1.right = merge(node1.right, node2)
+        node1.update()
+    }
+    else -> {
+        node2.left = merge(node1, node2.left)
+        node2.update()
+    }
+}
+
+class Treap {
+    private var root: TreapNode? = null
+
+    fun insert(value: Int) {
+        root = merge(root, TreapNode(value))
+    }
+
+    fun remove(position: Int): Int? {
+        val (left, other) = root.split(position)
+        val (removed, right) = other.split(position - left.size + 1)
+        root = merge(left, right)
+        return removed?.value
+    }
 }
 
 fun main() {
-    println(getGreeting())
+    val k = readInt()
+    val s = read()
+    val n = readInt()
+
+    val lettersWithPositions = HashMap<Char, Treap>()
+    s.repeat(k).asSequence().forEachIndexed { index, char ->
+        lettersWithPositions.getOrPut(char, ::Treap).insert(index)
+    }
+
+    val removedLettersPositions = HashSet<Int>()
+    for (iterator in 1..n) {
+        val (position, char) = readStrings().zipWithNext { p, c -> p.toInt() to c.first() }.first()
+        lettersWithPositions[char]?.remove(position - 1)?.let { removedLettersPositions.add(it) }
+    }
+
+    s.repeat(k).asSequence().filterIndexed { index, _ -> index !in removedLettersPositions }.forEach(::print)
+    println()
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -2,10 +2,92 @@ package ru.hse.spb
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.io.*
 
 class TestSource {
+    private fun testIO(input: String, output: String) {
+        val out = ByteArrayOutputStream()
+        System.setOut(PrintStream(out))
+        System.setIn(ByteArrayInputStream(input.toByteArray()))
+
+        main()
+        assertEquals(output, out.toString())
+
+        System.setIn(System.`in`)
+        System.setOut(System.out)
+    }
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun test1() {
+        testIO("""
+            2
+            bac
+            3
+            2 a
+            1 b
+            2 c
+        """.trimIndent(), "acb\n")
+    }
+
+    @Test
+    fun test2() {
+        testIO("""
+            1
+            abacaba
+            4
+            1 a
+            1 a
+            1 c
+            2 b
+        """.trimIndent(), "baa\n")
+    }
+
+    @Test
+    fun test3() {
+        testIO("""
+            10
+            bcbccaacab
+            40
+            37 c
+            21 a
+            18 a
+            5 b
+            1 a
+            8 c
+            9 a
+            38 c
+            10 b
+            12 c
+            18 a
+            23 a
+            20 c
+            7 b
+            33 c
+            4 c
+            22 c
+            28 c
+            9 a
+            12 a
+            22 a
+            1 b
+            6 a
+            31 c
+            19 b
+            19 a
+            15 a
+            6 c
+            11 c
+            18 b
+            19 c
+            24 c
+            8 a
+            16 c
+            2 c
+            12 b
+            8 a
+            14 c
+            18 b
+            19 c
+        """.trimIndent(), "cbcaabbccaaabbcccacabbccbbcbccabbcaacbbbcaacbccabbccaabbbcab\n")
     }
 }
