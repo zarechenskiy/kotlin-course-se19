5a93bbdb9a3c63a453372af32ccc155ecbd6fb1a
Krock21rus <krock21rus@gmail.com>
2019-09-11 00:04:09 +0300

    hw1 done

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,68 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.lang.AssertionError
 
-    return words.joinToString(separator = " ")
+val answerArray: ArrayList<Int> = ArrayList()
+var currentPosition: Int = 0
+
+fun getAnswerFromTd(input: String): Int {
+    var ans = 0
+    while (input.drop(currentPosition).startsWith("<td>")) {
+        currentPosition += 4
+        getAnswerFromTable(input)
+        ans++
+        if (!input.drop(currentPosition).startsWith("</td>")) {
+            throw AssertionError()
+        }
+        currentPosition += 5
+    }
+    return ans
+}
+
+fun getAnswerFromTr(input: String): Int {
+    var ans = 0
+    while (input.drop(currentPosition).startsWith("<tr>")) {
+        currentPosition += 4
+        ans += getAnswerFromTd(input)
+        if (!input.drop(currentPosition).startsWith("</tr>")) {
+            throw AssertionError()
+        }
+        currentPosition += 5
+    }
+    return ans
+}
+
+fun getAnswerFromTable(input: String): Int {
+    var ans = 0
+    if (input.drop(currentPosition).startsWith("<table>")) {
+        currentPosition += 7
+        ans = getAnswerFromTr(input)
+    } else return ans
+    if (!input.drop(currentPosition).startsWith("</table>")) {
+        throw AssertionError()
+    }
+    currentPosition += 8
+    answerArray.add(ans)
+    return 0
 }
 
 fun main() {
-    println(getGreeting())
+    var input = String()
+    var nextLine = readLine()
+    while (nextLine != null) {
+        input += nextLine
+        nextLine = readLine()
+    }
+    getAnswerFromTable(input)
+    answerArray.sorted().forEach { ans -> print(ans); print(" ") }
+}
+
+// for tests
+fun getAnswer(input: String): String {
+    answerArray.clear()
+    currentPosition = 0
+    getAnswerFromTable(input.filter { it != '\n' })
+    var answer = String()
+    answerArray.sorted().forEach { ans -> answer += ans; answer += " " }
+    return answer
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -4,8 +4,38 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class TestSource {
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun test51BSample1() {
+        assertEquals("1 ", getAnswer("<table><tr><td></td></tr></table>"))
     }
+
+    @Test
+    fun test51BSample2() {
+        assertEquals("1 4 ", getAnswer("""<table>
+<tr>
+<td>
+<table><tr><td></td></tr><tr><td></
+td
+></tr><tr
+><td></td></tr><tr><td></td></tr></table>
+</td>
+</tr>
+</table>
+"""))
+    }
+
+    @Test
+    fun test51BSample3() {
+        assertEquals("1 1 1 3 ", getAnswer("""<table><tr><td>
+<table><tr><td>
+<table><tr><td>
+<table><tr><td></td><td></td>
+</tr><tr><td></td></tr></table>
+</td></tr></table>
+</td></tr></table>
+</td></tr></table>
+"""))
+    }
+
 }
