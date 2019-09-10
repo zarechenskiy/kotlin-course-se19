ac1a0a72ecfa8b7bbbd442ac0a7e97053de4e6b2
Michael <michael.nik999@gmail.com>
2019-09-10 14:25:54 +0300

    add Scheduler with calculateCostOfDelay and part of changeSchedule and main

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,44 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
 
-    return words.joinToString(separator = " ")
-}
 
 fun main() {
-    println(getGreeting())
+    val scanner = Scanner(System.`in`)
+    val flightsNumber = scanner.nextInt()
+    val shift = scanner.nextInt()
+    val costOfDelay = generateSequence { scanner.nextInt() }.take(flightsNumber).toList().toIntArray()
+    val scheduler = Scheduler(costOfDelay)
+    val newSchedule = scheduler.changeSchedule(shift)
 }
+
+class Scheduler(private val costOfDelay: IntArray) {
+
+    private val flightsNumber = costOfDelay.size
+    fun changeSchedule(shift: Int): List<Int> {
+        val flightQueue = costOfDelay.take(shift).toSortedSet()
+        val newSchedule = generateSequence { -1 }.take(flightsNumber).toMutableList()
+
+        var currentIndex = shift
+        while (flightQueue.isNotEmpty()) {
+            if (currentIndex < flightsNumber) {
+                flightQueue.add(costOfDelay[currentIndex])
+            }
+
+        }
+        return newSchedule
+    }
+
+    fun calculateCostOfDaley(newSchedule: List<Int>): Long = newSchedule
+            .map { it.toLong() }
+            .withIndex()
+            .zip(newSchedule)
+            .map { (range, cost) ->
+                val (newTime, originTime) = range
+                (newTime - originTime) * cost
+            }
+            .sum()
+
+}
+
+data class Flights(val id: Int, val costOfDelay: Int)
