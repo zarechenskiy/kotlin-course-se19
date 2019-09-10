9c3a5f163172f4aa46d320dfd6ecf649ab7ca950
Darya Razumova <d.razumova@yandex.ru>
2019-09-11 01:25:28 +0300

    Solution of 854C

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,60 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.math.BigInteger
+import java.util.*
+import kotlin.Comparator
 
-    return words.joinToString(separator = " ")
+data class PlaneInfo(val delayCost: Int, val index: Int)
+data class DepartureOrder(val totalCost: BigInteger, val planesIndexes: List<Int>)
+
+private fun multiplyInBigInt(fisrt: Int, second: Int): BigInteger {
+    return BigInteger.valueOf((fisrt * second).toLong())
+}
+
+fun solve(planesNumber: Int, delay: Int, list: List<Int>): DepartureOrder {
+    val planesList = list.indices.map { PlaneInfo(list[it], it) }
+
+    val setOfAvailablePlanes = TreeSet(planesList.take(delay).toSortedSet(object : Comparator<PlaneInfo> {
+        override fun compare(o1: PlaneInfo, o2: PlaneInfo): Int {
+            val delayDist = o1.delayCost - o2.delayCost
+            val indexDist = o1.index - o2.index
+            if (delayDist != 0) {
+                return delayDist
+            }
+
+            return indexDist
+        }
+    }))
+    val departureTime = IntArray(planesNumber)
+    var resultSum = BigInteger.ZERO
+
+    for (time in (delay + 1..planesNumber)) {
+        setOfAvailablePlanes.add(planesList[time - 1])
+
+        val mostExpesivePlane = setOfAvailablePlanes.last()
+        resultSum += multiplyInBigInt(mostExpesivePlane.delayCost, time - mostExpesivePlane.index - 1)
+        setOfAvailablePlanes.remove(mostExpesivePlane)
+        departureTime[mostExpesivePlane.index] = time
+    }
+
+    for (time in (planesNumber + 1..delay + planesNumber)) {
+        val mostExpesivePlane = setOfAvailablePlanes.last()
+        resultSum += multiplyInBigInt(mostExpesivePlane.delayCost, time - mostExpesivePlane.index - 1)
+        setOfAvailablePlanes.remove(mostExpesivePlane)
+        departureTime[mostExpesivePlane.index] = time
+    }
+
+    return DepartureOrder(resultSum, departureTime.toList())
 }
 
 fun main() {
-    println(getGreeting())
+    val scanner = Scanner(System.`in`)
+    val number = scanner.nextInt()
+    val delay = scanner.nextInt()
+    val costs = (1..number).map { scanner.nextInt() }
+
+    val result = solve(number, delay, costs)
+
+    println(result.totalCost)
+    println(result.planesIndexes.joinToString(separator = " "))
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -2,10 +2,13 @@ package ru.hse.spb
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.math.BigInteger
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun firstTest() {
+        val result = solve(5, 2, listOf(4, 2, 1, 10, 2))
+        assertEquals(20, result.totalCost.intValueExact())
+        assertEquals(listOf(3, 6, 7, 4, 5), result.planesIndexes)
     }
 }
