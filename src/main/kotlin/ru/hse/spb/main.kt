1bff7c61631f6ce6d4b3d4a893a00e2b55a5f856
Vladislav <vmos1999@mail.ru>
2019-09-10 10:15:32 +0300

    HW1 done

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,121 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashSet
+import kotlin.math.PI
+import kotlin.math.atan
 
-    return words.joinToString(separator = " ")
+fun getSymbolsFromString(string : String) : HashSet<Char> {
+    val stringSymbols = HashSet<Char>()
+    for (symbol in string) {
+        if (!stringSymbols.contains(symbol)) {
+            stringSymbols.add(symbol)
+        }
+    }
+
+    return stringSymbols
+}
+
+fun getStringMissingSymbols(string: String) : ArrayList<Char> {
+    val stringSymbols = getSymbolsFromString(string)
+    val missingSymbols = ArrayList<Char>()
+
+    for (symbol in 'a'..'z') {
+        if (!stringSymbols.contains(symbol)) {
+            missingSymbols.add(symbol)
+        }
+    }
+
+    return missingSymbols
+}
+
+fun canChooseSymbolNumber(string: String) : Int {
+    var canChooseSymbolNumber = 0
+    for (i in 0 until (string.length + 1)/ 2) {
+        canChooseSymbolNumber +=
+                if (string[i] == '?' && string[string.length - 1 - i] == '?') {
+                    1
+                } else {
+                    0
+                }
+    }
+
+    return canChooseSymbolNumber
+}
+
+fun mustChooseSymbolNumber(missingSymbols : ArrayList<Char>, mustHaveCharsNumber: Int) : Int {
+    var mustChooseCharsNumber = 0
+    for (symbol in missingSymbols) {
+        if (symbol - 'a' < mustHaveCharsNumber) {
+            mustChooseCharsNumber++
+        }
+    }
+
+    return mustChooseCharsNumber
+}
+
+fun hasAnswer(string: String) : Boolean {
+    for (i in 0 until (string.length + 1) / 2) {
+        if (string[i] != '?' && string[string.length - 1 - i] != '?') {
+            if (string[i] != string[string.length - 1 - i]) {
+                return false
+            }
+        }
+    }
+
+    return true
+}
+
+fun solveProblem(string: String, mustHaveCharsNumber : Int) : ArrayList<Char>? {
+    val chars = ArrayList<Char>()
+    val missingSymbols = getStringMissingSymbols(string)
+    val mustChooseCharsNumber = mustChooseSymbolNumber(missingSymbols, mustHaveCharsNumber)
+    var canChooseCharsNumber = canChooseSymbolNumber(string)
+    var index = 0
+
+    if (mustChooseCharsNumber > canChooseCharsNumber || !hasAnswer(string)) {
+        return null
+    }
+
+    for (char in string) {
+        chars.add(char)
+    }
+
+    for (i in 0 until (string.length + 1) / 2) {
+        if (string[i] == '?' && string[string.length - 1 - i] == '?') {
+            if (mustChooseCharsNumber < canChooseCharsNumber) {
+                chars[i] = 'a'
+                chars[string.length - 1 - i] = 'a'
+                canChooseCharsNumber--
+            } else {
+                chars[i] = missingSymbols[index]
+                chars[string.length - 1 - i] = missingSymbols[index]
+                index++
+            }
+        } else if (string[i] == '?') {
+            chars[i] = string[string.length - 1 - i]
+            chars[string.length - 1 - i] = string[string.length - 1 - i]
+        } else {
+            chars[i] = string[i]
+            chars[string.length - 1 - i] = string[i]
+        }
+    }
+
+    return chars
 }
 
 fun main() {
-    println(getGreeting())
+    val reader = Scanner(System.`in`)
+    val mustHaveCharsNumber = reader.nextInt()
+    val string = reader.next()
+    val resultString = solveProblem(string, mustHaveCharsNumber)
+
+    if (resultString == null) {
+        print("IMPOSSIBLE")
+    } else {
+        for (char in resultString) {
+            print(char)
+        }
+    }
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,62 @@
 package ru.hse.spb
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Test
 
 class TestSource {
+
+    private fun listEquals(list1: List<Char>?, list2: List<Char>) : Boolean {
+        if (list1 == null) {
+            return false
+        }
+
+        if (list1.size != list2.size) {
+            return false
+        }
+
+        for (i in 0 until list1.size) {
+            if (list1[i] != list2[i]) {
+                return false
+            }
+        }
+
+        return true
+    }
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun test1() {
+        assertNull(solveProblem("a?c", 3))
+    }
+
+    @Test
+    fun test2() {
+        assertTrue(listEquals(solveProblem("a??a", 2), "abba".toList()))
+    }
+
+    @Test
+    fun test3() {
+        assertNull(solveProblem("a??a", 3))
+    }
+
+    @Test
+    fun test4() {
+        assertTrue(listEquals(solveProblem("?", 1), "a".toList()))
+    }
+
+    @Test
+    fun test5() {
+        assertTrue(listEquals(solveProblem("a??d?fgh???????beaabaih????c??", 9),
+                "aacdafghiabaaebbeaabaihgfadcaa".toList()))
+    }
+
+    @Test
+    fun test6() {
+        assertTrue(listEquals(solveProblem( "??cdef??i??lmn?pq??tuv???z?u????b??no??????vhv?v?w?v?vh????????????n???u?z??wv?ts????nm???ih?f???b?",
+                26), "abcdefahiaalmnapqastuvwaazauaaanbaanoaagjkrvhvxvywyvxvhvrkjgaaonaabnaaauazaawvutsaqpanmlaaihafedcba".toList()))
+    }
+
+    @Test
+    fun test7() {
+        assertNull(solveProblem("abcdeffcdfda", 6))
     }
 }
