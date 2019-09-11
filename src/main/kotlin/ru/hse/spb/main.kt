519f53aa0304671018591111fc86aae593c9f6b6
Nikita Andreev <nik310392illbeback@gmail.com>
2019-09-11 13:51:49 +0300

    Andreev Nikita HW01 Fixed

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -5,54 +5,69 @@ import java.io.OutputStream
 import java.io.PrintWriter
 import java.math.BigInteger
 import java.util.Scanner
-fun solveTask(input: InputStream, output: OutputStream) {
+
+open class Vector(val x: Int, val y: Int)
+
+class IndexedVector(val index: Int, x: Int, y: Int) : Vector(x, y)
+
+fun getData(input: InputStream): Array<IndexedVector> {
     val scan = Scanner(input)
     val n = scan.nextInt()
-    val data = Array<Pair<Int, Pair<Int, Int>>>(n) { Pair(0, Pair(0, 0)) }
-    for (i in 0 until n) 
-        data[i] = Pair(i + 1, Pair(scan.nextInt(), scan.nextInt()))
-    data.sortWith((Comparator<Pair<Int, Pair<Int, Int>>> { x, y -> vectorsComp(x.second, y.second) }))
-    var maxCos = Pair<BigInteger, BigInteger>(BigInteger.valueOf((-1).toLong()), BigInteger.ONE)
-    var answer = Pair(data[0].first, data[1].first)
+    val data = Array<IndexedVector>(n) { IndexedVector(0, 0, 0) }
+    for (i in 0 until n)
+        data[i] = IndexedVector(i + 1, scan.nextInt(), scan.nextInt())
+    return data
+}
+
+fun printAnswer(output: OutputStream, answer: String) {
+    val writer = PrintWriter(output)
+    writer.print(answer)
+    writer.close()
+}
+
+fun solveTask(input: InputStream, output: OutputStream) {
+    val data = getData(input)
+    val n = data.size
+    data.sortWith(Comparator { x, y -> vectorsComp(x, y) })
+    var maxCos = Pair<BigInteger, BigInteger>(-BigInteger.ONE, BigInteger.ONE)
+    var answer = Pair(data[0].index, data[1].index)
     for (i in 0 until n) {
-        val angleCos = angleCos(data[i].second, data[(i + 1) % n].second)
+        val angleCos = angleCos(data[i], data[(i + 1) % n])
         if (angleCos.first * maxCos.second > angleCos.second * maxCos.first) {
             maxCos = angleCos
-            answer = Pair(data[i].first, data[(i + 1) % n].first)
+            answer = Pair(data[i].index, data[(i + 1) % n].index)
         }
     }
-    val writer = PrintWriter(output)
-    writer.print("${answer.first} ${answer.second}")
-    writer.close()
+    printAnswer(output, "${answer.first} ${answer.second}")
 }
 
 fun main(args: Array<String>) {
     solveTask(System.`in`, System.out)
 }
 
-fun angleCos(v1: Pair<Int, Int>, v2: Pair<Int, Int>): Pair<BigInteger, BigInteger> {
+fun angleCos(v1: Vector, v2: Vector): Pair<BigInteger, BigInteger> {
     val scalarProduct = scalarProduct(v1, v2)
     return Pair(scalarProduct * scalarProduct * scalarProduct.signum().toBigInteger(), module(v1) * module(v2))
 }
 
-fun scalarProduct(v1: Pair<Int, Int>, v2: Pair<Int, Int>): BigInteger {
-    return (v1.first * v2.first + v1.second * v2.second).toBigInteger()
+fun scalarProduct(v1: Vector, v2: Vector): BigInteger {
+    return (v1.x * v2.x + v1.y * v2.y).toBigInteger()
 }
 
-fun module(v: Pair<Int, Int>): BigInteger {
+fun module(v: Vector): BigInteger {
     return scalarProduct(v, v)
 }
 
-fun vectorsComp(v1: Pair<Int, Int>, v2: Pair<Int, Int>): Int {
-    var sign = v1.second * v2.first - v1.first * v2.second
+fun vectorsComp(v1: Vector, v2: Vector): Int {
+    var sign = v1.y * v2.x - v1.x * v2.y
     if (sign == 0) {
-        if ((v1.first * v2.first < 0 || v1.second * v2.second < 0))
-            sign = if (v2.second != 0) v2.second else v2.first
+        if ((v1.x * v2.x < 0 || v1.y * v2.y < 0))
+            sign = if (v2.y != 0) v2.y else v2.x
     } else
         if (sign < 0) {
-            if (v1.second < 0 && v2.second >= 0) sign = 1
+            if (v1.y < 0 && v2.y >= 0) sign = 1
         } else {
-            if (v1.second >= 0 && v2.second < 0) sign = -1
+            if (v1.y >= 0 && v2.y < 0) sign = -1
         }
     return sign
 }
