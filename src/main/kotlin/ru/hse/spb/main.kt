6d3a014cfb7e7588850eaf6ee94fc9272b2e7a05
Maria Tigina <tigmah@mail.ru>
2019-09-11 21:55:17 +0300

    01-just-for-fun: Added implementation for problem D. Kilani and the Game

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,124 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
 
-    return words.joinToString(separator = " ")
+const val CHAR_DIGIT_ONE = '1'
+
+enum class FieldStatus {
+    EMPTY, BLOCKED, CASTLE
 }
 
-fun main() {
-    println(getGreeting())
+val dx = intArrayOf(1, 0, 0, -1)
+val dy = intArrayOf(0, 1, -1, 0)
+
+data class Field(
+        var x: Int,
+        var y: Int
+) {
+    var status: FieldStatus? = null
+    var distance: Int = 0
+
+    fun setStatus(statusSymbol: Char) {
+        when (statusSymbol) {
+            '#' -> this.status = FieldStatus.BLOCKED
+            '.' -> this.status = FieldStatus.EMPTY
+            else -> {
+                this.status = FieldStatus.CASTLE
+            }
+        }
+    }
+
+    val isFree: Boolean
+        get() = status == FieldStatus.EMPTY
+}
+
+data class Player(
+        val id: Int,
+        val maxDistance: Int
+) {
+    var castles: Queue<Field> = LinkedList()
+    var score: Int = 0
+
+    fun addCastle(field: Field, distance: Int = 0) {
+        field.status = FieldStatus.CASTLE
+        field.distance = distance
+        castles.add(field)
+        this.score++
+    }
+}
+
+data class Board(
+        var rows: Int,
+        var cols: Int
+) {
+    private val board: Array<Array<Field>> = Array(rows) { row -> Array(cols) { col -> Field(row, col) } }
+
+    fun containsField(i: Int, j: Int): Boolean {
+        return i in 0 until rows && j in 0 until cols
+    }
+
+    fun getField(i: Int, j: Int): Field {
+        return board[i][j]
+    }
+}
+
+fun playGame(board: Board, players: Array<Player>): List<Int> {
+    while (players.any { it.castles.isNotEmpty() }) {
+        for (player in players) {
+
+            for (castle in player.castles) {
+                castle.distance = 0
+            }
+
+            val newCastles = LinkedList<Field>()
+            while (!player.castles.isEmpty()) {
+                val castle = player.castles.poll()
+                if (castle.distance == player.maxDistance) {
+                    newCastles.add(castle)
+                    continue
+                }
+                for (j in 0..3) {
+                    val x = castle.x + dx[j]
+                    val y = castle.y + dy[j]
+                    if (board.containsField(x, y) && board.getField(x, y).isFree) {
+                        player.addCastle(board.getField(x, y), castle.distance + 1)
+                    }
+                }
+            }
+            player.castles = newCastles
+        }
+    }
+    return players.map { player -> player.score }
+}
+
+fun initGame(scanner: Scanner): Pair<Board, Array<Player>> {
+    val rows = scanner.nextInt()
+    val cols = scanner.nextInt()
+    val playersNumber = scanner.nextInt()
+
+    val players: Array<Player> = Array(playersNumber) { playerId -> Player(playerId, scanner.nextInt()) }
+    val board = Board(rows, cols)
+
+    val fieldSymbols: Array<CharArray> = Array(rows) { scanner.next().toCharArray() }
+
+    for ((i, fieldSymbolRow) in fieldSymbols.withIndex()) {
+        for ((j, fieldSymbol) in fieldSymbolRow.withIndex()) {
+            val field = board.getField(i, j)
+            field.setStatus(fieldSymbol)
+            if (field.status == FieldStatus.CASTLE) {
+                players[fieldSymbol - CHAR_DIGIT_ONE].addCastle(board.getField(i, j))
+            }
+        }
+    }
+    return Pair(board, players)
+}
+
+fun runGameCycle(scanner: Scanner) {
+    val (board, players) = initGame(scanner)
+    val scores = playGame(board, players)
+    print(scores.joinToString(" "))
+}
+
+fun main(args: Array<String>) {
+    runGameCycle(Scanner(System.`in`))
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -4,8 +4,50 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class TestSource {
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun firstGameTest() {
+        val rows = 3
+        val cols = 3
+        val playersNumber = 2
+
+        val players: Array<Player> = Array(playersNumber) { playerId -> Player(playerId, 1) }
+        val board = Board(rows, cols)
+
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                val field = board.getField(i, j)
+                field.setStatus('.')
+            }
+        }
+
+        players[0].addCastle(board.getField(0, 0))
+        players[1].addCastle(board.getField(2, 2))
+
+        assertEquals("First test failed", listOf(6, 3), playGame(board, players))
+    }
+
+    @Test
+    fun secondGameTest() {
+        val rows = 3
+        val cols = 4
+        val playersNumber = 4
+
+        val players: Array<Player> = Array(playersNumber) { playerId -> Player(playerId, 1) }
+        val board = Board(rows, cols)
+
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                val field = board.getField(i, j)
+                field.setStatus('.')
+            }
+        }
+        board.getField(1, 0).setStatus('#')
+
+        for ((j, player) in players.withIndex()) {
+            player.addCastle(board.getField(2, j))
+        }
+
+        assertEquals("Second test failed", listOf(1, 4, 3, 3), playGame(board, players))
     }
 }
