95c5e00eef0a67f4d61d4a0d8f2c461992022823
Vladislav Platonov <v.platonov@phoenixcas.com>
2019-09-12 23:19:25 +0300

    First task

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,41 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
 
-    return words.joinToString(separator = " ")
+fun getGoodSubstrNumber(input : String) : Int {
+    var previousZeros = 0
+    var goodSubstrNumber = 0
+    for ((index, elem) in input.withIndex()) {
+        var maxLength = previousZeros + 1
+        if (elem == '1') {
+            var currentNumber = 0
+            for (forwardIndex in index until input.length) {
+                currentNumber *= 2
+                if (input[forwardIndex] == '1') {
+                    ++currentNumber
+                }
+
+                if (currentNumber <= maxLength) {
+                    ++goodSubstrNumber
+                } else {
+                    break
+                }
+                ++maxLength
+            }
+            previousZeros = 0
+        } else {
+            ++previousZeros
+        }
+    }
+    return goodSubstrNumber
 }
 
 fun main() {
-    println(getGreeting())
+    val reader = Scanner(System.`in`)
+    val queriesNumber:Int = reader.nextInt()
+    reader.nextLine()
+    for (query in 0 until queriesNumber) {
+        val input = reader.nextLine()
+        println(getGoodSubstrNumber(input))
+    }
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -5,7 +5,27 @@ import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testGoodSubstr1() {
+        assertEquals(4, getGoodSubstrNumber("0110"))
+    }
+
+    @Test
+    fun testGoodSubstr2() {
+        assertEquals(3, getGoodSubstrNumber("0101"))
+    }
+
+    @Test
+    fun testGoodSubstr3() {
+        assertEquals(4, getGoodSubstrNumber("00001000"))
+    }
+
+    @Test
+    fun testGoodSubstr4() {
+        assertEquals(3, getGoodSubstrNumber("0001000"))
+    }
+
+    @Test
+    fun testZeroString() {
+        assertEquals(0, getGoodSubstrNumber("0000000"))
     }
 }
