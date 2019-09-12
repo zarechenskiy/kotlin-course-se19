6ecfe2c9707999acec17d49bbf5a45db5b19e654
Maria Malysheva <mn.malysheva@mail.ru>
2019-09-12 21:40:34 +0300

    HW1 just-for-fun

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,102 @@
-package ru.hse.spb
+import java.lang.IllegalStateException
+import java.lang.IndexOutOfBoundsException
+import java.util.*
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+class Metro(numberOfStations: Int) {
+    private val stations: Array<Station>
+    private var tunnelsLeft: Int
 
-    return words.joinToString(separator = " ")
+    init {
+        stations = Array(numberOfStations + 1) { Station() }
+        tunnelsLeft = numberOfStations
+    }
+
+    class Station {
+        var neighborStations = arrayListOf<Station>()
+        var visited = false
+        var isOnCircleRoute = false
+        var distanceFromCircleRoute = -1
+
+        fun markCircleRoute(parent: Station, path: Stack<Station>): Boolean {
+            path.push(this)
+            if (visited) {
+                do {
+                    path.pop().isOnCircleRoute = true
+                } while (path.peek() != this)
+                return true
+            }
+
+            visited = true
+            for (neighbour in neighborStations) {
+                if (neighbour != parent && neighbour.markCircleRoute(this, path)) {
+                    return true
+                }
+            }
+            path.pop()
+            return false
+        }
+
+        fun calculateDistancesFromCircleRoute(distance: Int = 0) {
+            if (distanceFromCircleRoute != -1) {
+                return
+            }
+            distanceFromCircleRoute = if (isOnCircleRoute) 0 else distance
+            for (neighbour in neighborStations) {
+                neighbour.calculateDistancesFromCircleRoute(distanceFromCircleRoute + 1)
+            }
+        }
+    }
+
+    private fun checkIndex(index: Int) {
+        if (index <= 0 || index > stations.size) {
+            throw IndexOutOfBoundsException("Station index must be in range [1, numberOfStations]")
+        }
+    }
+
+    fun addTunnel(firstStationIndex: Int, secondStationIndex: Int): Metro {
+        if (tunnelsLeft == 0) {
+            throw IllegalStateException("Number of tunnels already exceeded number of stations");
+        }
+        tunnelsLeft--
+        checkIndex(firstStationIndex)
+        checkIndex(secondStationIndex)
+
+        val firstStation = stations[firstStationIndex]
+        val secondStation = stations[secondStationIndex]
+        firstStation.neighborStations.add(secondStation)
+        secondStation.neighborStations.add(firstStation)
+        return this
+    }
+
+    fun calculateDistancesFromCircleRoute() {
+        if (tunnelsLeft > 0) {
+            throw IllegalStateException("Number of tunnels is less than number of stations")
+        }
+
+        val path = Stack<Station>()
+        stations.last().markCircleRoute(stations.first(), path)
+        for (station in stations) {
+            if (station.isOnCircleRoute) {
+                station.calculateDistancesFromCircleRoute()
+                break
+            }
+        }
+    }
+
+    fun getDistanceFromCircleRoute(stationIndex: Int) = stations[stationIndex].distanceFromCircleRoute
 }
 
 fun main() {
-    println(getGreeting())
+    with(Scanner(System.`in`)) {
+        val numberOfStations = nextInt()
+        val metro = Metro(numberOfStations)
+        for (i in 1..numberOfStations) {
+            metro.addTunnel(nextInt(), nextInt())
+        }
+        metro.calculateDistancesFromCircleRoute()
+        for (i in 1..numberOfStations) {
+            print(metro.getDistanceFromCircleRoute(i))
+            print(' ')
+        }
+    }
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,55 @@
 package ru.hse.spb
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Test
+import java.lang.IllegalStateException
 
-class TestSource {
+class TestMetro {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testOneCircle() {
+        val metro = Metro(3).addTunnel(1, 2).addTunnel(1, 3).addTunnel(3, 2)
+        metro.calculateDistancesFromCircleRoute()
+        assertEquals(0, metro.getDistanceFromCircleRoute(1))
+        assertEquals(0, metro.getDistanceFromCircleRoute(2))
+        assertEquals(0, metro.getDistanceFromCircleRoute(3))
+    }
+
+    @Test
+    fun testComplex() {
+        val metro = Metro(10)
+                .addTunnel(6, 1).addTunnel(5, 4).addTunnel(2, 3).addTunnel(2, 9).addTunnel(7, 2)
+                .addTunnel(2, 6).addTunnel(1, 7).addTunnel(7, 5).addTunnel(4, 8).addTunnel(4, 10)
+        metro.calculateDistancesFromCircleRoute()
+        assertEquals(0, metro.getDistanceFromCircleRoute(1))
+        assertEquals(0, metro.getDistanceFromCircleRoute(2))
+        assertEquals(0, metro.getDistanceFromCircleRoute(6))
+        assertEquals(0, metro.getDistanceFromCircleRoute(7))
+        assertEquals(1, metro.getDistanceFromCircleRoute(3))
+        assertEquals(1, metro.getDistanceFromCircleRoute(9))
+        assertEquals(1, metro.getDistanceFromCircleRoute(5))
+        assertEquals(2, metro.getDistanceFromCircleRoute(4))
+        assertEquals(3, metro.getDistanceFromCircleRoute(8))
+        assertEquals(3, metro.getDistanceFromCircleRoute(10))
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun testTooManyTunnels() {
+        Metro(4).addTunnel(1, 2).addTunnel(1, 3).addTunnel(3, 2).addTunnel(1, 4).addTunnel(4, 2)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun testNotEnoughTunnels() {
+        val metro = Metro(4).addTunnel(1, 2).addTunnel(1, 3).addTunnel(3, 2)
+        metro.calculateDistancesFromCircleRoute()
+    }
+
+    @Test(expected = IndexOutOfBoundsException::class)
+    fun testZeroIndex() {
+        Metro(4).addTunnel(0, 2)
+    }
+
+    @Test(expected = IndexOutOfBoundsException::class)
+    fun testTooLargeIndex() {
+        Metro(4).addTunnel(1, 5)
     }
 }
