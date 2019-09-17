d97da934cbd05560f25742340b707ce0d403e663
Vladislav Tankov <vdtankov@gmail.com>
2019-09-17 12:34:03 +0300

    Fixes after review

--- src/main/kotlin/tanvd/itmo/kotlin/Main.kt
+++ src/main/kotlin/tanvd/itmo/kotlin/Main.kt
@@ -5,45 +5,39 @@ import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
-object Solver {
-    fun solve(input: InputStream): String {
+fun solve(input: InputStream): String {
 
-        val newString = Array(200001) { ' ' }
-        val lettersToIndices = HashMap<Char, ArrayList<Int>>()
+    val newString = Array(200001) { ' ' }
+    val lettersToIndices = HashMap<Char, ArrayList<Int>>()
 
-        return Scanner(input.bufferedReader()).use { scanner ->
-            val k = scanner.nextInt()
-            val string = scanner.next()
-            val n = scanner.nextInt()
+    return Scanner(input.bufferedReader()).use { scanner ->
+        val k = scanner.nextInt()
+        val string = scanner.next()
+        val n = scanner.nextInt()
 
-            var p = 0
-            for (i in 0 until k) {
-                for (char in string) {
-                    newString[p] = char
-                    lettersToIndices.getOrPut(char, { ArrayList() }).add(p)
-                    p++
-                }
-            }
-
-            for (i in 0 until n) {
-                val aInd = scanner.nextInt() - 1
-                val b = scanner.nextChar()
-
-                newString[lettersToIndices[b]!![aInd]] = '!'
-                lettersToIndices[b]!!.removeAt(aInd)
-            }
-
-            buildString {
-                newString.asSequence()
-                        .take(p)
-                        .filter { it != '!' }
-                        .forEach {
-                            append(it)
-                        }
+        var p = 0
+        for (i in 0 until k) {
+            for (char in string) {
+                newString[p] = char
+                lettersToIndices.getOrPut(char, { ArrayList() }).add(p)
+                p++
             }
         }
+
+        for (i in 0 until n) {
+            val aInd = scanner.nextInt() - 1
+            val b = scanner.nextChar()
+
+            newString[lettersToIndices[b]!![aInd]] = '!'
+            lettersToIndices[b]!!.removeAt(aInd)
+        }
+
+        newString
+                .take(p)
+                .filter { it != '!' }
+                .joinToString(separator = "")
     }
 }
 
-fun main() = println(Solver.solve(System.`in`))
+fun main() = println(solve(System.`in`))
 
