5b42ac7f390e7ba984e44528c73e26586ceac717
surkov-m <surkov-m@yandex-team.ru>
2019-09-26 14:29:03 +0300

    fix main

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -2,7 +2,6 @@ package ru.hse.spb
 
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
-import org.antlr.v4.runtime.Token
 import ru.hse.spb.parser.LangLexer
 import ru.hse.spb.parser.LangParser
 import java.io.File
@@ -11,35 +10,10 @@ fun removeComments(code: List<String>): List<String> {
     return code.map { line -> line.substring(0, ("$line//").indexOf("//")) }
 }
 
-private fun getTypes(code: String): List<Int> {
-    val lexer = LangLexer(CharStreams.fromString(code))
-
-    val tokens: MutableList<Token> = ArrayList()
-
-    while (!lexer._hitEOF) {
-        val token = lexer.nextToken()
-        if (token.type == LangParser.WS) {
-            continue
-        }
-        tokens.add(token)
-    }
-
-    for (t in tokens) {
-        println("LangParser.${LangParser.VOCABULARY.getSymbolicName(t.type)},")
-    }
-
-    return tokens.map { it.type }
-}
-
 fun main(args: Array<String>) {
-//    require(args.size == 1) { "Only one argument was expected, but ${args.size} found" }
-//    val code = removeComments(File(args[0]).readLines()).joinToString(separator = System.lineSeparator())
-//    val lexer = LangLexer(CharStreams.fromString(code))
-//    val parser = LangParser(CommonTokenStream(lexer))
-//    parser.file().block().accept(BlockVisitor())
-    val code =
-        "var a = 10\n" +
-                "println((a + 1) / (a - 1)) * 101 % 3)\n"
-
-    getTypes(code)
+    require(args.size == 1) { "Only one argument was expected, but ${args.size} found" }
+    val code = removeComments(File(args[0]).readLines()).joinToString(separator = System.lineSeparator())
+    val lexer = LangLexer(CharStreams.fromString(code))
+    val parser = LangParser(CommonTokenStream(lexer))
+    parser.file().block().accept(BlockVisitor())
 }
