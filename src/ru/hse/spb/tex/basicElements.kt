3ece9f2bcb7f18ae629817db6059b51bb025b182
Vladilav Yepifanov <evn1999@yandex.ru>
2019-10-01 15:57:17 +0300

    moved a bit more & fixed imports

--- src/ru/hse/spb/tex/basicElements.kt
+++ src/ru/hse/spb/tex/basicElements.kt
@@ -1,5 +1,6 @@
 package ru.hse.spb.tex
 
+import ru.hse.spb.tex.util.ItemGenerator
 import ru.hse.spb.tex.util.pairsToParameter
 import java.io.Writer
 
@@ -71,4 +72,22 @@ open class Statements : Elements() {
         addElement(Tag(tag + pairsToParameter(parameter)), init)
 
     fun command(command: String) = +"\\$command"
+}
+
+open class Tag(private val name: String) : Statements() {
+    override fun render(output: Writer, indent: String) {
+        output.appendln("$indent\\begin{$name}")
+        super.render(output, indent + "\t")
+        output.appendln("$indent\\end{$name}")
+    }
+}
+
+open class ItemTag(private val name: String) : Elements() {
+    val item = ItemGenerator(this)
+
+    override fun render(output: Writer, indent: String) {
+        output.appendln("$indent\\begin{$name}")
+        super.render(output, indent + "\t")
+        output.appendln("$indent\\end{$name}")
+    }
 }
