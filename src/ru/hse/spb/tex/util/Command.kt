f6ae60e450994801f207399ce8d36ce4ec11ab55
Vladilav Yepifanov <evn1999@yandex.ru>
2019-10-02 14:04:23 +0300

    extracted CommandInitializer

--- src/ru/hse/spb/tex/util/Command.kt
+++ src/ru/hse/spb/tex/util/Command.kt
@@ -1,8 +1,6 @@
 package ru.hse.spb.tex.util
 
 import ru.hse.spb.tex.Element
-import ru.hse.spb.tex.Elements
-import ru.hse.spb.tex.Statements
 import java.io.Writer
 
 open class CommandWithBody<T : Element>(val text: String, protected val body: T) : Element {
@@ -40,35 +38,3 @@ open class Command(text: String) : CommandWithBody<Command.EmptyElement>(text, E
         override fun render(output: Writer, indent: String) {}
     }
 }
-
-open class CommandInitializer<Y : Element, T : CommandWithBody<Y>>(val command: T) {
-
-    operator fun get(vararg params: String): CommandInitializer<Y, T> {
-        command.addSquareArguments(*params)
-        return this
-    }
-    operator fun get(param: Pair<String, String>, vararg params: Pair<String, String>): CommandInitializer<Y, T> =
-        get(pairsToParameter(param, *params))
-
-    operator fun invoke(vararg figureParams: String): CommandInitializer<Y, T> {
-        command.addFigureArguments(*figureParams)
-        return this
-    }
-
-    operator fun invoke(commandInit: Y.() -> Unit) {
-        command.initBody(commandInit)
-    }
-
-    operator fun invoke(vararg figureParams: String, commandInit: Y.() -> Unit) {
-        invoke(*figureParams)
-        invoke(commandInit)
-    }
-}
-
-open class CommandWithoutBodyInitializer(
-    commandName: String,
-    commandConsumer: (Command) -> Any
-)
-    : CommandInitializer<Command.EmptyElement, Command>(
-    Command(commandName).also { commandConsumer(it) }
-)
---
+++ src/ru/hse/spb/tex/util/CommandInitializer.kt
@@ -0,0 +1,35 @@
+package ru.hse.spb.tex.util
+
+import ru.hse.spb.tex.Element
+
+open class CommandInitializer<Y : Element, T : CommandWithBody<Y>>(val command: T) {
+
+    operator fun get(vararg params: String): CommandInitializer<Y, T> {
+        command.addSquareArguments(*params)
+        return this
+    }
+    operator fun get(param: Pair<String, String>, vararg params: Pair<String, String>): CommandInitializer<Y, T> =
+        get(pairsToParameter(param, *params))
+
+    operator fun invoke(vararg figureParams: String): CommandInitializer<Y, T> {
+        command.addFigureArguments(*figureParams)
+        return this
+    }
+
+    operator fun invoke(commandInit: Y.() -> Unit) {
+        command.initBody(commandInit)
+    }
+
+    operator fun invoke(vararg figureParams: String, commandInit: Y.() -> Unit) {
+        invoke(*figureParams)
+        invoke(commandInit)
+    }
+}
+
+open class CommandWithoutBodyInitializer(
+    commandName: String,
+    commandConsumer: (Command) -> Any
+)
+    : CommandInitializer<Command.EmptyElement, Command>(
+    Command(commandName).also { commandConsumer(it) }
+)
