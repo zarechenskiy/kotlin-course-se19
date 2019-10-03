1d7ec42415b7d65d3ac5d16250e89a120f5baa2e
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-10-03 18:56:06 +0300

    add lists

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ interface Element {
 
 abstract class Tag(
     private val tagName: String,
-    private val arg: String
+    private val arg: String?
 ): Element {
 
     override fun toOutputStream(stream: OutputStream, indent: String) {
@@ -16,10 +16,26 @@ abstract class Tag(
     }
 }
 
+@DslMarker
+annotation class TexTagMarker
+
+@TexTagMarker
 abstract class TagWithBlock(
     private val tagName: String,
     protected val children: MutableList<Element> = ArrayList()
 ): Element {
+
+    protected fun <T: Element> initTag(tag: T, init: T.() -> Unit): T {
+        tag.init()
+        this.children.add(tag)
+        return tag
+    }
+
+    fun frame(init: Frame.() -> Unit) = initTag(Frame(), init)
+
+    fun itemize(init: Itemize.() -> Unit) = initTag(Itemize(), init)
+
+    fun enumerate(init: Enumerate.() -> Unit) = initTag(Enumerate(), init)
 
     override fun toOutputStream(stream: OutputStream, indent: String) {
         stream.write("$indent\\begin{$tagName}${System.lineSeparator()}".toByteArray())
@@ -31,17 +47,46 @@ abstract class TagWithBlock(
 }
 
 class DocumentClass(docClass: String): Tag("documentclass", docClass)
+class Package(name: String): Tag("usepackage", name)
+class FrameTitle(title: String): Tag("frametitle", title)
+class Item: TagWithBlock("item") {
+
+    override fun toOutputStream(stream: OutputStream, indent: String) {
+        stream.write("$indent\\item${System.lineSeparator()}".toByteArray())
+        for (child in children) {
+            child.toOutputStream(stream, "$indent    ")
+        }
+    }
+}
+
+class Frame: TagWithBlock("frame") {
+
+    fun frametitle(title: String) {
+        val frameTitle = FrameTitle(title)
+        this.children.add(frameTitle)
+    }
+}
+
+class Itemize: TagWithBlock("itemize") {
+
+    fun item(init: Item.() -> Unit) = initTag(Item(), init)
+}
+class Enumerate: TagWithBlock("enumerate") {
+
+    fun item(init: Item.() -> Unit) = initTag(Item(), init)
+}
 
 class Document: TagWithBlock("document") {
 
-    private var docClass: DocumentClass? = null
-
-    fun documentClass(docClass: String): DocumentClass {
+    fun documentClass(docClass: String) {
         val documentClass = DocumentClass(docClass)
-        check(this.docClass == null) { "Can not call documentClass more than one time" }
-        this.docClass = documentClass
-        this.children.add(this.docClass!!)
-        return documentClass
+        this.children.add(documentClass)
+    }
+
+    fun usepackage(vararg packages: String) {
+        for (p in packages) {
+            this.children.add(Package(p))
+        }
     }
 
     fun toOutputStream(stream: OutputStream) {
@@ -57,6 +102,21 @@ fun document(init: Document.() -> Unit): Document {
 
 fun main() {
     document {
+        usepackage("babel", "russian", "minted", "enumerate")
         documentClass("beamer")
+        enumerate {
+            item {
+
+            }
+            frame {
+                frame {
+                    enumerate {
+                        item {
+
+                        }
+                    }
+                }
+            }
+        }
     }.toOutputStream(System.out)
 }
