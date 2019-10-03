6a2b153d3161c9aef6b82cffe9a32fbb69307293
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-10-03 18:02:19 +0300

    add document nd doc class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,62 @@
 package ru.hse.spb
 
-import kotlin.collections.ArrayList
-import java.*
+import java.io.OutputStream
+
+interface Element {
+    fun toOutputStream(stream: OutputStream, indent: String)
+}
+
+abstract class Tag(
+    private val tagName: String,
+    private val arg: String
+): Element {
+
+    override fun toOutputStream(stream: OutputStream, indent: String) {
+        stream.write("$indent\\$tagName{$arg}${System.lineSeparator()}".toByteArray())
+    }
+}
+
+abstract class TagWithBlock(
+    private val tagName: String,
+    protected val children: MutableList<Element> = ArrayList()
+): Element {
+
+    override fun toOutputStream(stream: OutputStream, indent: String) {
+        stream.write("$indent\\begin{$tagName}${System.lineSeparator()}".toByteArray())
+        for (child in children) {
+            child.toOutputStream(stream, "$indent    ")
+        }
+        stream.write("$indent\\end{$tagName}${System.lineSeparator()}".toByteArray())
+    }
+}
+
+class DocumentClass(docClass: String): Tag("documentclass", docClass)
+
+class Document: TagWithBlock("document") {
+
+    private var docClass: DocumentClass? = null
+
+    fun documentClass(docClass: String): DocumentClass {
+        val documentClass = DocumentClass(docClass)
+        check(this.docClass == null) { "Can not call documentClass more than one time" }
+        this.docClass = documentClass
+        this.children.add(this.docClass!!)
+        return documentClass
+    }
+
+    fun toOutputStream(stream: OutputStream) {
+        super.toOutputStream(stream, "")
+    }
+}
+
+fun document(init: Document.() -> Unit): Document {
+    val document = Document()
+    document.init()
+    return document
+}
 
 fun main() {
-    println("Hello world")
-    val a = ArrayList<Int>()
-
+    document {
+        documentClass("beamer")
+    }.toOutputStream(System.out)
 }
